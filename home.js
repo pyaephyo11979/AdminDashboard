d6389ad7cc761db0985414aed4086e4a2f60c9c6
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-08-05 11:47:58 +0630

    Add frameworks

--- home.js
+++ home.js
@@ -1,5 +1,34 @@
 let container=document.querySelector("#body");
 let themeChangeBtn=document.getElementById("dLbtn");
+let homeBtn=document.getElementById('homeBtn');
+let usersBtn=document.getElementById('usersBtn');
+let staticsBtn=document.getElementById('staticsBtn');
+let booksBtn=document.getElementById('booksBtn');
+homeBtn.addEventListener('click',()=>{
+    homeBtn.classList.add('action');
+    usersBtn.classList.remove('action');
+    staticsBtn.classList.remove('action');
+    booksBtn.classList.remove('action');
+})
+usersBtn.addEventListener('click',()=>{
+    usersBtn.classList.add('action');
+    homeBtn.classList.remove('action');
+    staticsBtn.classList.remove('action');
+    booksBtn.classList.remove('action');
+})
+staticsBtn.addEventListener('click',()=>{
+    staticsBtn.classList.add('action');
+    usersBtn.classList.remove('action');
+    homeBtn.classList.remove('action');
+    booksBtn.classList.remove('action');
+})
+booksBtn.addEventListener('click',()=>{
+    booksBtn.classList.add('action');
+    usersBtn.classList.remove('action');
+    staticsBtn.classList.remove('action');
+    homeBtn.classList.remove('action');
+})
+
 function dark(){
     container.setAttribute('data-bs-theme','dark');
 }
